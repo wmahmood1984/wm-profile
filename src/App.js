2ec92ff2263fcecf8f0caee70f3cfd6a8f250357
Waqas Mahmood <waqasnizamani3@gmail.com>
2021-04-25 12:24:38 +0500

    text animation added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
+import {AnimatedText} from './AnimatedText'
 
 function App() {
   return (
     <div className="App">
+      <AnimatedText
+      textColor="#cd122d"
+      overlayColor="fdc52c"
+      >
       
+
       Waqas Mahmood
+      </AnimatedText>
+
       
     </div>
   );
