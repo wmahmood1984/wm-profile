db59016c6e787d6d1dc814782cac86124faed9de
Waqas Mahmood <waqasnizamani3@gmail.com>
2021-04-26 20:36:03 +0500

    changes made in card

--- src/components/Paper.js
+++ src/components/Paper.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import PortfolioReviewCard from './Card';
+import Portfolio from '../portfolio.json'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
+    flexDirection: 'column',
     flexWrap: 'wrap',
     '& > *': {
-      margin: theme.spacing(1),
+      margin: theme.spacing(2),
       width: theme.spacing(160),
       height: theme.spacing(160),
     },
@@ -17,10 +20,22 @@ const useStyles = makeStyles((theme) => ({
 export default function SimplePaper() {
   const classes = useStyles();
 
+  console.log('result',Portfolio[0].url)
+
   return (
     <div className={classes.root}>
       
-      <Paper elevation={3} />
+      <Paper elevation={3}>
+        {Portfolio.map((item,i)=>(
+          <PortfolioReviewCard
+          key={i}
+          title = {item.title}
+          url={item.url}
+          cardContent = {item.description}
+          ></PortfolioReviewCard>
+        ))}
+          
+      </Paper>
     </div>
   );
 }
