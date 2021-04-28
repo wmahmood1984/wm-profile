af4a1524ffad969e7f7eff972374d0fb14b132f6
Waqas Mahmood <waqasnizamani3@gmail.com>
2021-04-28 18:56:46 +0500

    List of portfolio completed

--- src/PortfolioList.js
+++ src/PortfolioList.js
@@ -3,6 +3,18 @@ import a from './images/PetShopReact.jpg'
 import b from './images/Drizzle-cryptoKitty.jpg'
 import c from './images/ExpenseTrackerTruffle.jpg'
 import d from './images/MaterialUI.jpg'
+import e from './images/Virtual_Lolly.png'
+import f from './images/Gatsby_Diary.png'
+import g from './images/JamStack_GraphQl.png'
+import h from './images/ContentFull.png'
+import i from './images/ShoppingBasket.png'
+import j from './images/SpaceX.png'
+import k from './images/Quiz.png'
+import l from './images/ScrollAnimate.png'
+import m from './images/SelfAnimate.png'
+import n from './images/AliceInWonderLand.png'
+import o from './images/ShoeStore.png'
+import p from './images/Covid.png'
 
 
 const List = [
@@ -40,7 +52,101 @@ const List = [
         "picture": d,
         "description": "These are the boilerPlate website of forms and Tables using Material UI components with details make to fit components.",
         "url": "http://material-ui-wm.surge.sh/"
+      },
+      {
+        "id": 5,
+        "title": "React Native Apps",
+        "picture": "Will be available on Demand",
+        "description": "These are mobile apps prepared on React Native and React Natiave base. Both through traditional serverbase set-ups and AWS serverless",
+        "url": "Will be available on Demand"
+      },
+      {
+        "id": 6,
+        "title": "Virtual Lolly with Gatsby",
+        "picture": e,
+        "description": "This app is prepared using gatsby and customized web pages are generated at build time using web hooks",
+        "url": "https://quizzical-williams-4f9b3c.netlify.app/"
+      },
+      {
+        "id": 7,
+        "title": "Gatsby Diary",
+        "picture": f,
+        "description": "This app is prepared using gatsby. FaunaDb database is used to store the data and its retrieval. ",
+        "url": "https://gatsby-diary-wm.netlify.app/"
+      },
+      {
+        "id": 8,
+        "title": "JamStack Serverless -Netlify",
+        "picture": g,
+        "description": "This is gatsby app using Netlify serverless functions and faunaDb. Graphql is used for APIs ",
+        "url": "https://jamstack-todo-wm.netlify.app/"
+      },
+      {
+        "id": 9,
+        "title": "BlogSpot Using ContentFul",
+        "picture": h,
+        "description": "This App is using contentful for its content management. Front-end is designed in gatsby and Graphql is used for API ",
+        "url": "https://gatsby-contentful-blogpost.netlify.app/blog/"
+      },
+      {
+        "id": 10,
+        "title": "Shopping Basket",
+        "picture": i,
+        "description": "This app has been created using react and react redux store  ",
+        "url": "https://shoppingbasket2wm.surge.sh/"
+      },
+      {
+        "id": 11,
+        "title": "Space X App",
+        "picture": j,
+        "description": "This app has been designed using GraphQl technologies and Apollo Server. Front end is designed using react  ",
+        "url": "https://spacexwm.netlify.app/"
+      },
+      {
+        "id": 12,
+        "title": "Quiz App",
+        "picture": k,
+        "description": "This App has been designed in React using Typescript. Rest APIs are used in this app for backend integration ",
+        "url": "http://quizappwm.surge.sh/"
+      },
+      {
+        "id": 13,
+        "title": "Scroll Animation app",
+        "picture": l,
+        "description": "This app has been created using React animation tools. Pre-dominantly this app was to build to highlight animation during scrolling ",
+        "url": "http://animatedsite.surge.sh/"
+      },
+      {
+        "id": 14,
+        "title": "Self Animation app",
+        "picture": m,
+        "description": "This app has been created using React animation tools. Animation controls are used to control the animation on web page ",
+        "url": "http://selfanimation4b.surge.sh/"
+      },
+      {
+        "id": 15,
+        "title": "Alice in Wonder Land",
+        "picture": n,
+        "description": "This app has been created using React animation tools. Animation controls are used to control the animation on web page ",
+        "url": "http://aliceinwonderland-react.surge.sh/"
+      },
+      {
+        "id": 16,
+        "title": "Shoe Store",
+        "picture": o,
+        "description": "This app has been created using React navigation tools.",
+        "url": "http://shoestorewm.surge.sh/"
+      },
+      {
+        "id": 17,
+        "title": "Covid Tracker App",
+        "picture": p,
+        "description": "The idea of this was to create a react app using rest apis and JavaScript Graphs",
+        "url": "http://covid19trackerwm.surge.sh/"
       }
+
+
+
 
   ]
 
